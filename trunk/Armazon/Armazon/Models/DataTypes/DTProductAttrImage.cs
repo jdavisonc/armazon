50846096758f0a638ed2c9b7579d836e3c453777
jorge.sofpc <jorge.sofpc@569ff150-22f7-11de-8d76-854d91f79a93>
2009-05-09 20:04:50 +0000

    subo nuevos dt

---
+++ trunk/Armazon/Armazon/Models/DataTypes/DTProductAttrImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Armazon.Models.DataTypes
+{
+    public class DTProductAttrImage
+    {
+        private byte[] _imagen;
+
+        public byte[] Imagen
+        {
+            get { return _imagen; }
+            set { _imagen = value; }
+        }
+
+        public DTProductAttrImage()
+        {
+        }
+    }
+}
